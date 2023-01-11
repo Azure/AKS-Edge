e50f2850817496e84740a5cd028403ba2ea3d1c9
Paramesh Babu <pabab@microsoft.com>
2023-01-10 16:19:25 -0800

    ver update

--- tools/scripts/AksEdgeAzureSetup/AksEdgeAzureSetup-Test.ps1
+++ tools/scripts/AksEdgeAzureSetup/AksEdgeAzureSetup-Test.ps1
@@ -6,7 +6,7 @@ Param(
 )
 
 #Requires -RunAsAdministrator
-New-Variable -Name gAksEdgeAzureSetupTest -Value "1.0.221208.0900" -Option Constant -ErrorAction SilentlyContinue
+New-Variable -Name gAksEdgeAzureSetupTest -Value "1.0.230109.1600" -Option Constant -ErrorAction SilentlyContinue
 
 function Install-AzCli {
     #Check if Az CLI is installed. If not install it.
