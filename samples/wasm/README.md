86490171661e8ae82e407c006c8b2085a0cdd456
Francisco Cabrera <fcabrera@microsoft.com>
2023-02-22 11:31:32 -0500

    Adding enable/disable and tested with K3s

--- samples/wasm/README.md
+++ samples/wasm/README.md
@@ -1,34 +1,57 @@
-# Orchestrate WASM workloads using AKS Edge
+# Orchestrate WASM workloads using AKS Edge Essentials
 
 ## Introduction
 
-This sample demonstrates how to run a WebAssembly (WASM) payload using [spin-containerd-wasm-shim](https://github.com/deislabs/containerd-wasm-shims) inside the AKS Edge cluster. **containerd-wasm-shim** is currently in _alpha_ version and is not intended for production deployments.
+This sample demonstrates how to run a WebAssembly (WASM) payload using [containerd-wasm-shim](https://github.com/deislabs/containerd-wasm-shims) inside an AKS Edge Essentials cluster.
+**containerd-wasm-shim** only supports Linux nodes; Winodws nodes support is under development.
+
+ _:warning: **WARNING**_: _This sample is experimental only and is not intended for production deployments. **containerd-wasm-shim** is currently in **alpha** version._
 
 ## Prerequisites
 
-Check [AKS Edge Essentials requirements and support matrix](https://learn.microsoft.com/en-us/azure/aks/hybrid/aks-edge-system-requirements).
+Check [AKS Edge Essentials requirements and support matrix](https://learn.microsoft.com/azure/aks/hybrid/aks-edge-system-requirements).
 
 ## Instructions
 
 1. Setup AKS Edge Essentials - Follow [Set up machine](https://aka.ms/aks-edge/quickstart)
-2. Open an elevated PowerShell session
-3. Download and load [Enable-AksEdgeWasmWorkload.ps1](./Enable-AksEdgeWasmWorkloads.ps1)
-4. Run the `Enable-AksEdgeWasmWorkload` cmdlet to enable the *containerd-wasm-shim*. By default version **v0.3.3** is used.
+1. Deploy a [Single Machine cluster](https://learn.microsoft.com/azure/aks/hybrid/aks-edge-howto-single-node-deployment) or a [Scalable Cluster](https://learn.microsoft.com/azure/aks/hybrid/aks-edge-howto-multi-node-deployment).
+1. Open an elevated PowerShell session
+1. Move to an appropriate working directory
+1. Download [Set-AksEdgeWasmRuntime.ps1](./Set-AksEdgeWasmRuntimes.ps1)
+    ```powershell
+    Invoke-WebRequest -Uri "https://raw.githubusercontent.com/Azure/AKS-Edge/wasm-enablement/samples/wasm/Set-AksEdgeWasmRuntimes.ps1" -OutFile ".\Set-AksEdgeWasmRuntimes.ps1"
+    ```
+4. Run the `Set-AksEdgeWasmRuntime` cmdlet to enable the *containerd-wasm-shim*. By default, version **v0.3.3** is used.
 
     ```powershell
-    .\Enable-AksEdgeWasmWorkloads.ps1
+    .\Set-AksEdgeWasmRuntime.ps1 -enable
     ```
 
-5. Apply a runtime class that contains a handler that matches the "spin" config runtime name from previous step.
+   | Parameter | Options | Description | 
+   | --------- | ------- | ----------- |
+   | enable | None | If this flag is present, the command enables the feature.|
+   | shimOption | spin, slight, both | containerd-wasm-shim version. For more information, see https://github.com/deislabs/containerd-wasm-shims |
+   | shimVersion | None | containerd-wasm-shim version. For more information, see https://github.com/deislabs/containerd-wasm-shims |
+    
+
+5. Apply the *runtime.yaml* to create the *wasmtime-slight* and *wasmtime-spin* rumtime classes.
 
     ```powershell
     kubectl apply -f https://github.com/deislabs/containerd-wasm-shims/releases/download/v0.3.3/runtime.yaml
     ```
+    
+    If everything was correctly created, you should see the two runtime classes.
 
-6. Deploy a Wasm workload to your cluster with the specified runtime class name matching the "wasmtime-spin" runtime class from previous step.
+    ```bash
+    NAME              HANDLER   AGE
+    wasmtime-slight   slight    5s
+    wasmtime-spin     spin      5s
+    ```
+
+6. Deploy Wasm workloads to your cluster using the *wasmtime-spin* and *wasmtime-slight* runtime classes deployed in the previous step.
 
     ```powershell
-    kubectl apply -f https://raw.githubusercontent.com/fcabrera23/AKS-Edge/wasm-enablement/samples/wasm/workload.yaml
+    kubectl apply -f https://raw.githubusercontent.com/Azure/AKS-Edge/wasm-enablement/samples/wasm/workload.yaml
     ```
 
 7. Check that the pods are deployed and running
@@ -40,7 +63,7 @@ Check [AKS Edge Essentials requirements and support matrix](https://learn.micros
     If everything was correctly configured, you should see four wasm pods running. If pods are not running, use kubectl describe pods <name-of-pod> to get further troubleshooting information.
 
     ```bash
-   NAME                           READY   STATUS    RESTARTS   AGE
+    NAME                           READY   STATUS    RESTARTS   AGE
     wasm-slight-66849b8575-5l2zv   1/1     Running   0          12s
     wasm-spin-bd6d84876-4bcs5      1/1     Running   0          12s
     wasm-spin-bd6d84876-fpmh2      1/1     Running   0          12s
@@ -68,7 +91,7 @@ Check [AKS Edge Essentials requirements and support matrix](https://learn.micros
     Invoke-AksEdgeNodeCommand -NodeType Linux -command "curl -v http://<wasm-spin/slight-ip-address>:<wasm-spin/slight-port>/hello"
     ```
 
-    If everything is running correctly, you should see the following output when using Slight
+    If everything runs correctly, you should see the following output when using Slight.
 
     ```bash
     hello
@@ -78,6 +101,18 @@ Check [AKS Edge Essentials requirements and support matrix](https://learn.micros
 
     ```bash
     Hello world from Spin!
+    ```
+
+## Clean up deployment
+
+Once you're finished with WASM workloads, clean up your workspace by running the following commands.
+
+1. Open an elevated PowerShell session  
+1. Delete all resources
+    ```powershell
+    kubectl delete -f https://raw.githubusercontent.com/Azure/AKS-Edge/wasm-enablement/samples/wasm/workload.yaml
+    kubectl delete -f https://github.com/deislabs/containerd-wasm-shims/releases/download/v0.3.3/runtime.yaml
+    .\Set-AksEdgeWasmRuntime.ps1
     ```
 
 ## Feedback
---
+++ samples/wasm/Set-AksEdgeWasmRuntime.ps1
@@ -0,0 +1,200 @@
+<#
+    .SYNOPSIS
+        Enables or disables WASM support on AKS Edge Essentials
+
+    .DESCRIPTION
+        The Set-AksEdgeWasmRuntime cmdlet enables or disables WASM support inside AKS Edge Essentials Linux node
+
+    .PARAMETER enable
+        If this flag is present, the WASM rutime is enabled, otherwise is disabled.
+    
+    .PARAMETER shimVersion
+        containerd-wasm-shim version. For more information, see https://github.com/deislabs/containerd-wasm-shims
+
+    .PARAMETER shimOption
+        WASM Engine shim: Spin, Slight, or both. For more information, see https://github.com/deislabs/containerd-wasm-shims
+
+    .EXAMPLE
+        Set-AksEdgeWasmRuntime -enable
+
+    .LINK
+        https://github.com/Azure/AKS-Edge/tree/wasm-enablement/samples/wasm 
+#>
+
+param(
+    [Switch] $enable,
+    [string] $shimVersion = "v0.3.3",
+    [ValidateSet("spin", "slight", "both")]
+    [string] $shimOption = "both"
+)
+
+Write-Host "1. Checking AKS Edge Essentials dependencies" -ForegroundColor Green
+$IsK8s = $false
+$version = (Get-ChildItem -Path 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\' | Get-ItemProperty |  Where-Object {$_.DisplayName -like 'AKS Edge Essentials*'}).DisplayName
+if ([string]::IsNullOrEmpty($version))
+{
+    Write-Host "AKS Edge Essentials is not installed on this device" -ForegroundColor Red
+    return
+}
+
+if ($version.Contains("K8s"))
+{ 
+    Write-Host "    K8s version found" -ForegroundColor Cyan
+    $IsK8s = $true
+}
+elseif ($version.Contains("K3s"))
+{ 
+    Write-Host "    K3s version found" -ForegroundColor Cyan
+}
+else
+{
+    throw $("AKS Edge Essentials verison not supported")
+}
+
+if ($enable.IsPresent)
+{
+    Write-Host "2. Downloading shim verison $shimVersion" -ForegroundColor green
+    Invoke-AksEdgeNodeCommand "wget -O /home/aksedge-user/containerd-wasm-shim.tar.gz https://github.com/deislabs/containerd-wasm-shims/releases/download/$shimVersion/containerd-wasm-shims-v1-linux-x86_64.tar.gz"
+
+    Write-Host "3. Unpacking and moving shim to appropiate folder" -ForegroundColor green
+    Invoke-AksEdgeNodeCommand "tar -xvf /home/aksedge-user/containerd-wasm-shim.tar.gz && sudo mkdir /var/lib/bin" -ignoreError | Out-Null
+
+    if($shimOption -eq "both")
+    {
+        Invoke-AksEdgeNodeCommand "sudo mv /home/aksedge-user/containerd-shim-spin-v1 /var/lib/bin/ && sudo mv /home/aksedge-user/containerd-shim-slight-v1 /var/lib/bin/ && sudo rm /home/aksedge-user/containerd-*"  | Out-Null
+    }
+    else
+    {
+        Invoke-AksEdgeNodeCommand "sudo mv /home/aksedge-user/containerd-shim-$shimOption-v1 /var/lib/bin/ && sudo rm /home/aksedge-user/containerd-*"  | Out-Null
+    }
+
+    Write-Host "4. Copying required files" -ForegroundColor green
+    if($IsK8s)
+    {
+        Invoke-AksEdgeNodeCommand -NodeType Linux "sudo cp /etc/containerd/config.toml /home/aksedge-user/config.toml"
+    }
+    else
+    {
+        Invoke-AksEdgeNodeCommand -NodeType Linux "sudo cp /var/lib/rancher/k3s/agent/etc/containerd/config.toml /home/aksedge-user/config.toml"
+    }
+
+    Invoke-AksEdgeNodeCommand -NodeType Linux "sudo chown -R aksedge-user /home/aksedge-user/config.toml"
+    Copy-AksEdgeNodeFile -NodeType Linux -FromFile "/home/aksedge-user/config.toml" -ToFile ".\config.toml"
+
+    Write-Host "5. Configuring containerd config files to support runwasi runtime" -ForegroundColor green
+    if($shimOption -eq "both")
+    {
+        $command = "`n[plugins.cri.containerd.runtimes.spin]`n  runtime_type = ""io.containerd.spin.v1""`n[plugins.cri.containerd.runtimes.slight]`n  runtime_type = ""io.containerd.slight.v1"""
+        if($IsK8s)
+        {
+            $command =  "`n[plugins.""io.containerd.grpc.v1.cri"".containerd.runtimes.slight]`n  runtime_type = ""io.containerd.slight.v1""`n[plugins.""io.containerd.grpc.v1.cri"".containerd.runtimes.spin]`n  runtime_type = ""io.containerd.spin.v1"""
+        }
+        Add-Content -Path ".\config.toml" $command     
+    }
+    else
+    {
+        $command = "`n[plugins.cri.containerd.runtimes.$shimOption]`n  runtime_type = ""io.containerd.$shimOption.v1"""
+        if($IsK8s)
+        {
+            $command =  "`n[plugins.""io.containerd.grpc.v1.cri"".containerd.runtimes.$shimOption]`n  runtime_type = ""io.containerd.$shimOption.v1"""
+        }
+        Add-Content -Path ".\config.toml" $command  
+    }
+
+    Copy-AksEdgeNodeFile -NodeType Linux -FromFile ".\config.toml" -ToFile "/home/aksedge-user/config.toml" -PushFile
+    if($IsK8s)
+    {
+        Invoke-AksEdgeNodeCommand -NodeType Linux "sudo cp /home/aksedge-user/config.toml /etc/containerd/config.toml"   
+    }
+    else
+    {
+        Invoke-AksEdgeNodeCommand -NodeType Linux "sudo cp /home/aksedge-user/config.toml /var/lib/rancher/k3s/agent/etc/containerd/config.toml.tmpl"
+    }
+
+    Write-Host "6. Cleaning unnecessary files" -ForegroundColor green
+    Invoke-AksEdgeNodeCommand -NodeType Linux "sudo rm /home/aksedge-user/config.toml"
+    Remove-Item -Path ".\config.toml"
+
+    Write-Host "7. Adding new runwasi directory to  PATH variable" -ForegroundColor green
+    $currentPath = '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin'
+    $newPath = "PATH=" + $currentPath + ":/var/lib/bin"
+    Write-Host "    Current PATH=$currentPath - New $newPath" -ForegroundColor Cyan
+    $kubeService = "k3s"
+    if($IsK8s)
+    {
+        $kubeService = "containerd"
+    }
+
+    Write-Host "8. Configuring $kubeService service with new configuration" -ForegroundColor green
+    Invoke-AksEdgeNodeCommand -NodeType Linux "sudo cp /etc/systemd/system/$kubeService.service.d/override.conf /home/aksedge-user/override.conf"
+    Invoke-AksEdgeNodeCommand -NodeType Linux "sudo chown -R aksedge-user /home/aksedge-user/override.conf"
+    Copy-AksEdgeNodeFile -NodeType Linux -FromFile "/home/aksedge-user/override.conf" -ToFile ".\override.conf"
+    $command = "Environment=""$newPath"""
+    Add-Content -Path ".\override.conf" $command  
+    Copy-AksEdgeNodeFile -NodeType Linux -FromFile ".\override.conf" -ToFile "/home/aksedge-user/override.conf" -PushFile
+    Invoke-AksEdgeNodeCommand -NodeType Linux "sudo cp /home/aksedge-user/override.conf /etc/systemd/system/$kubeService.service.d/override.conf"
+
+    Write-Host "9. Cleaning unnecessary files" -ForegroundColor green
+    Invoke-AksEdgeNodeCommand -NodeType Linux "sudo rm /home/aksedge-user/override.conf"
+    Remove-Item -Path ".\override.conf"
+
+    Write-Host "10. Reloading and restarting services" -ForegroundColor green
+    Invoke-AksEdgeNodeCommand "sudo systemctl daemon-reload"
+    Invoke-AksEdgeNodeCommand "sudo systemctl restart $kubeService"
+
+    Write-Host "11. Configuration finished - You can now deploy WASM workloads using kubectl interface" -ForegroundColor green
+}
+else
+{
+    Write-Host "2. Remvoving shims from /var/lib/bin folder" -ForegroundColor green
+    if($shimOption -eq "both")
+    {
+        Invoke-AksEdgeNodeCommand "sudo rm /var/lib/bin/containerd-shim-spin-v1 && sudo rm /var/lib/bin/containerd-shim-slight-v1" -ignoreError | Out-Null
+    }
+    else
+    {
+        Invoke-AksEdgeNodeCommand "sudo rm /var/lib/bin/containerd-shim-$shimOption-v1" -ignoreError  | Out-Null
+    }
+
+    Write-Host "3. Removing containerd config files to support runwasi runtime" -ForegroundColor green
+    if($shimOption -eq "both")
+    {
+        if($IsK8s)
+        {
+            Invoke-AksEdgeNodeCommand -NodeType "Linux" -command "sudo sed -i -e '/spin/,+1d' /etc/containerd/config.toml" | Out-Null
+            Invoke-AksEdgeNodeCommand -NodeType "Linux" -command "sudo sed -i -e '/slight/,+1d' /etc/containerd/config.toml" | Out-Null
+        }
+        else
+        {
+            Invoke-AksEdgeNodeCommand -NodeType "Linux" -command "sudo sed -i -e '/spin/,+1d' /var/lib/rancher/k3s/agent/etc/containerd/config.toml.tmpl" | Out-Null
+            Invoke-AksEdgeNodeCommand -NodeType "Linux" -command "sudo sed -i -e '/slight/,+1d' /var/lib/rancher/k3s/agent/etc/containerd/config.toml.tmpl" | Out-Null
+        }    
+    }
+    else
+    {
+        if($IsK8s)
+        {
+            Invoke-AksEdgeNodeCommand -NodeType "Linux" -command "sudo sed -i -e '/${shimOption}/,+1d' /etc/containerd/config.toml" | Out-Null
+        }
+        else
+        {
+            Invoke-AksEdgeNodeCommand -NodeType "Linux" -command "sudo sed -i -e '/${shimOption}/,+1d' /var/lib/rancher/k3s/agent/etc/containerd/config.toml.tmpl" | Out-Null
+        }  
+    }
+
+    $kubeService = "k3s"
+    if($IsK8s)
+    {
+        $kubeService = "containerd"
+    }
+
+    Write-Host "3. Cleaning PATH environment config file" -ForegroundColor green
+    Invoke-AksEdgeNodeCommand -NodeType Linux "sudo sed -i -e '/PATH/d' /etc/systemd/system/$kubeService.service.d/override.conf"
+
+    Write-Host "4. Reloading and restarting services" -ForegroundColor green
+    Invoke-AksEdgeNodeCommand "sudo systemctl daemon-reload"
+    Invoke-AksEdgeNodeCommand "sudo systemctl restart $kubeService"
+}
+
+
+
