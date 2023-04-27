c573e49e0350865777ec7c9f712e7dd6a75bf3dd
ag17sep <garganimesh513@gmail.com>
2023-04-27 22:35:28 +0530

    Import windows-test in initialization script

--- tests/e2e.ps1
+++ tests/e2e.ps1
@@ -99,6 +99,7 @@ $aksedgeShell = (Get-ChildItem -Path "$AideModulePath" -Filter AksEdgeShell.ps1 
 
 . "$PSScriptRoot\E2E\e2e_basiclinuxoffline_test.ps1"
 . "$PSScriptRoot\E2E\e2e_basiclinuxonline_test.ps1"
+. "$PSScriptRoot\E2E\e2e_basiclinuxandwindowsoffline_test.ps1"
 
 # Put all commands into $CommandTree, which is a map(GroupName => Array(FunctionNames))
 $AllCommands = Get-Command -Verb 'E2eTest'
