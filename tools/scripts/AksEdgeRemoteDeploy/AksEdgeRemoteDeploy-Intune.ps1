81b34831bb2e27c9acbaeb89f5327431a57d1555
Francisco Cabrera <fcabrera@microsoft.com>
2023-05-15 19:19:21 -0400

    Revert version to original previous one

--- tools/scripts/AksEdgeRemoteDeploy/AksEdgeRemoteDeploy-Intune.ps1
+++ tools/scripts/AksEdgeRemoteDeploy/AksEdgeRemoteDeploy-Intune.ps1
@@ -25,7 +25,7 @@ param(
     [string] $Tag
 )
 #Requires -RunAsAdministrator
-New-Variable -Name gAksEdgeRemoteDeployVersion -Value "1.0.230515.1000" -Option Constant -ErrorAction SilentlyContinue
+New-Variable -Name gAksEdgeRemoteDeployVersion -Value "1.0.230321.1000" -Option Constant -ErrorAction SilentlyContinue
 if (! [Environment]::Is64BitProcess) {
     Write-Host "Error: Run this in 64bit Powershell session" -ForegroundColor Red
     exit -1
