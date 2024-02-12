f56062c758850c575ca7dab6b8f2fe5f0b858928
umallineni <umallineni@microsoft.com>
2024-02-12 15:20:06 -0800

    retval added for function call

--- tools/modules/AksEdgeDeploy/AksEdgeDeploy.psm1
+++ tools/modules/AksEdgeDeploy/AksEdgeDeploy.psm1
@@ -1118,7 +1118,11 @@ function Start-AideWorkflow {
         } else {
             Write-Host "-------------------going to outer else block-------------------"
             $jsonFile = (Resolve-Path -Path $jsonFile).Path
-            Set-AideUserConfig -jsonFile $jsonFile # validate later after creating the switch
+            $retval = Set-AideUserConfig -jsonFile $jsonFile # validate later after creating the switch
+            if (!$retval) {
+                Write-Host "Error: $jsonFile incorrect after creating switch" -ForegroundColor Red
+                return $false
+            }
         }
     }
 
