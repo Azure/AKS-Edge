176393d01c196d94c8438569704c7b78c0a949cd
umallineni <umallineni@microsoft.com>
2024-02-12 15:31:48 -0800

    removed write host statements used for testing

--- tools/modules/AksEdgeDeploy/AksEdgeDeploy.psm1
+++ tools/modules/AksEdgeDeploy/AksEdgeDeploy.psm1
@@ -1116,7 +1116,6 @@ function Start-AideWorkflow {
                 return $false
             }
         } else {
-            Write-Host "-------------------going to outer else block-------------------"
             $jsonFile = (Resolve-Path -Path $jsonFile).Path
             $retval = Set-AideUserConfig -jsonFile $jsonFile # validate later after creating the switch
             if (!$retval) {
