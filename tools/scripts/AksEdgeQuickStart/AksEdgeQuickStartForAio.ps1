0e6efe88d6cdfe6315077f9323625c02a610dcc0
Jagadish Murugan <jagamu@microsoft.com>
2025-02-26 18:58:59 -0800

    proceed with AIO Deployment only if AKSEE K8s is not already installed

--- tools/scripts/AksEdgeQuickStart/AksEdgeQuickStartForAio.ps1
+++ tools/scripts/AksEdgeQuickStart/AksEdgeQuickStartForAio.ps1
@@ -208,10 +208,24 @@ param(
     }
 }
 
+function Get-AkseeInstalledProductName
+{
+
+    return (Get-ChildItem -Path 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\' | Get-ItemProperty |  Where-Object {$_.DisplayName -like "*Aks Edge Essentials*"}).DisplayName
+}
+
 if (! [Environment]::Is64BitProcess) {
     Write-Host "Error: Run this in 64bit Powershell session" -ForegroundColor Red
     exit -1
 }
+
+$installedAkseeProductName = Get-AkseeInstalledProductName
+if (-Not [string]::IsNullOrEmpty($installedAkseeProductName)) {
+    if ($installedAkseeProductName -like "*K8s*") {
+        Write-Host "Detected AKSEE k8s installation. Please uninstall and run the script again!" -ForegroundColor Red
+    }
+}
+
 #Validate inputs
 if ($arcLocations -inotcontains $Location) {
     Write-Host "Error: Location $Location is not supported for Azure Arc" -ForegroundColor Red
